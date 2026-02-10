52b30fed2ee86324cc5cbb2de1a4baf73fcfad7e
4lex2218 <76576162+4lex2218@users.noreply.github.com>
2026-02-09 22:04:02 -0500

    2.8
    ejercicio 2.8 donde medimos la velocidad de los corredores dependiendo que tanto tiempo les asignemos

---
+++ guia 1/2.8 .py
@@ -0,0 +1,39 @@
+def calcular_velocidad_corredores():
+    print("Cálculo de Velocidad Media (1.500m)")
+    print("Ingrese el tiempo (0 0 para finalizar)")
+    
+    DISTANCIA = 1500 
+    
+    while True:
+        
+        entrada = input("Tiempo del corredor (minutos segundos): ").split()
+        
+        # verificamos los dos valores
+        if len(entrada) != 2:
+            print("Por favor, ingrese dos números.")
+            continue
+            
+        minutos = int(entrada[0])
+        segundos = int(entrada[1])
+        
+        
+        if minutos == 0 and segundos == 0:
+            print("Fin de entrada de datos.")
+            break
+            
+        # convertimos el tiempo a segundos
+        tiempo_total_segundos = (minutos * 60) + segundos
+        
+        if tiempo_total_segundos > 0:
+            #calculo
+            velocidad = DISTANCIA / tiempo_total_segundos
+            
+            print(f"\nRESULTADOS:")
+            print(f"Tiempo: {minutos} min {segundos} s")
+            print(f"Velocidad Media: {velocidad:.2f} m/s")
+            print("-" * 30)
+        else:
+            print("El tiempo debe ser mayor a cero.")
+
+
+calcular_velocidad_corredores()
